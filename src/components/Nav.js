53f9749124814e1e8d1e4f3d7dfcf520e0a4919c
sharananurag998 <sharananurag998@gmail.com>
2020-09-07 12:52:47 +0530

    Addded trailer popups

--- src/components/Nav.js
+++ src/components/Nav.js
@@ -26,7 +26,7 @@ function Nav() {
       <img
         className="nav_avatar"
         src="https://i.pinimg.com/originals/0d/dc/ca/0ddccae723d85a703b798a5e682c23c1.png"
-        alt="Avatar Image"
+        alt="Avatar"
       />
     </div>
   );
--- src/components/Row.js
+++ src/components/Row.js
@@ -1,10 +1,22 @@
 import React, { useState, useEffect } from "react";
 import axios from "../axios";
 import "./Row.css";
+import Youtube from "react-youtube";
+
 const baseImgUrl = "https://image.tmdb.org/t/p/original";
 
 function Row({ title, fetchUrl, isLargeRow }) {
   const [movies, setMovies] = useState([]);
+  const [trailerUrl, setTrailerUrl] = useState("");
+
+  // Options for react-youtube
+  const opts = {
+    height: "390",
+    width: "100%",
+    playerVars: {
+      autoplay: 1,
+    },
+  };
 
   useEffect(() => {
     async function fetchData() {
@@ -14,6 +26,17 @@ function Row({ title, fetchUrl, isLargeRow }) {
     }
     fetchData();
   }, [fetchUrl]);
+
+  const handleClick = async (movie) => {
+    if (trailerUrl) {
+      setTrailerUrl("");
+    } else {
+      let trailerurl = await axios.get(
+        `/movie/${movie.id}/videos?api_key=fb34530271b349314af0de263d16ab5a`
+      );
+      setTrailerUrl(trailerurl.data.results[0]?.key);
+    }
+  };
 
   return (
     <div className="row">
@@ -30,10 +53,12 @@ function Row({ title, fetchUrl, isLargeRow }) {
                 }`}
                 alt={movie.name}
                 key={movie.id}
+                onClick={() => handleClick(movie)}
               />
             )
         )}
       </div>
+      {trailerUrl && <Youtube videoId={trailerUrl} opts={opts} />}
     </div>
   );
 }
